6528fcadecbea05de3e21144dac75ec03edda719
Appaso Sanjay Avaghadi <appaso1919@gmail.com>
2025-05-16 09:58:24 +0530

    Creat hooks using  UseState method

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Favorite_color from './UseState_hooks'
+
+function App() {
+  
+  return (
+    <div>
+      <h3>Hooks in useState</h3>
+      <Favorite_color />
+    </div>
+  )
+}
+
+export default App
---
+++ src/UseState_hooks.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+function Favorite_color(){
+    const [color,setColor]=useState("red");
+    return(
+        <div>
+            
+            <h3>My favorite color is {color}</h3>
+            <button style={{backgroundColor:"blue",
+                
+            }} onClick={()=>setColor("blue")}>Blue</button><br/><br/>
+            <button style={{backgroundColor:"Red"}} onClick={()=>setColor("red")}>Red</button><br/><br/>
+            <button style={{backgroundColor:"orange",padding:"10px"
+            }} onClick={()=>setColor("orange")}>Orange</button><br/><br/>
+            <button style={{backgroundColor:"pink"}} onClick={()=>setColor("pink")}>Pink</button>
+        </div>
+    )
+}
+
+export default Favorite_color;
