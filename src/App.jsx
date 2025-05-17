e908611f9ece0356a09f09553e2539dbf08b66f2
Appaso Sanjay Avaghadi <appaso1919@gmail.com>
2025-05-17 11:04:09 +0530

    event handling using button

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import First from './class_component/first'
 import Array_loop from './Loop_in_React_with_array/array_loop'
 import Nested_loop_array from './Loop_in_React_with_array/nested_loop_array'
 import Simple_props from './Props/simple_props'
-import Click_event from './Event_handling/Click_event-basic'
+import Click_event from './Event_handling/Click_button'
 import UserList from './List_Key/User_list'
 
 function App() {
-  
+
   return (
     <div>
       <h1>Hooks</h1>
@@ -25,16 +25,16 @@ function App() {
       <h1>Class Component</h1>
       <First />
       <h1>Loops in React using Array</h1>
-      <Array_loop/>
+      <Array_loop />
       <h3>Nested loop using array</h3>
-      <Nested_loop_array/>
+      <Nested_loop_array />
       <h1>Props in React</h1>
-      <Simple_props name="Appaso"/>
-      <Simple_props name="Ajay"/>
+      <Simple_props name="Appaso" />
+      <Simple_props name="Ajay" />
       <h1>Event Handling</h1>
       <Click_event />
       <h1>List and key</h1>
-      <UserList/>
+      <UserList />
     </div>
   )
 }
